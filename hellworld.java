e71f2e48b390b197a5d9e9a6e51b261489b3e501
yuanbao <1309877947@qq.com>
2016-03-17 16:33:42 +0800

    add two lines codes-hello.java

--- hellworld.java
+++ hellworld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello world!");
+		System.out.println("modify file");
+		System.out.println("no add to short area");
 	}
 }
